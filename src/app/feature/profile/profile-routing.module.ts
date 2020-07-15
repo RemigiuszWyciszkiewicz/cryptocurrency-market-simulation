3644042e53c7ebb4c8f8dd81da95d53655e896a6
Remigiusz Wyciszkiewicz <remigiusz.wyciszkiewicz@asseco.pl>
2020-07-15 11:43:15 +0200

    prfile module generated

--- src/app/feature/profile/profile-routing.module.ts
+++ src/app/feature/profile/profile-routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './profile.component';
 
-const routes: Routes = [
-  {
-    path: 'pages',
-    loadChildren: () =>
-      import('../feature/profile/index').then((m) => m.ProfileModule),
-  },
-];
+const routes: Routes = [{ path: '', component: ProfileComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class PagesRoutingModule {}
+export class ProfileRoutingModule {}
