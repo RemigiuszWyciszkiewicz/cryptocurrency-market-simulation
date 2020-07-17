83b242c30482c01bba371692fae08d8c43f92368
Remigiusz Wyciszkiewicz <remigiusz.wyciszkiewicz@asseco.pl>
2020-07-17 22:32:33 +0200

    crypto api error response

--- server/controller/cryptocurrency.controller.js
+++ server/controller/cryptocurrency.controller.js
@@ -1,9 +1,14 @@
-const cryptoApi = require("../cryptocurrency-clients").coingeckoApi;
+const cryptoApi = require('../cryptocurrency-clients').coingeckoApi;
 
 const getAll = async (res, req) => {
+  try {
     const data = await cryptoApi.getAll();
-
-    res.res.send(data.data);
+    if (data.data) {
+      res.res.send(data.data);
+    }
+  } catch (error) {
+    res.res.send('Can not fetch coins list.');
+  }
 };
 
 module.exports = { getAll };
--- server/routers/authorization.route.js
+++ server/routers/authorization.route.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-
+const authorizationController = require('../controller').authorizationController;
 const router = Router();
 
 // router.post('/signup', passport.authenticate('signup', { session: false }), (req, res) => {
@@ -9,42 +9,8 @@ const router = Router();
 //   });
 // });
 
-router.post('/signup', async (req, res, next) => {
-  passport.authenticate('signup', async (err, user, info) => {
-    if (user) {
-      res.send(user);
-    }
+router.post('/signup', authorizationController.passportAuthenticateSignUp);
 
-    if (!user) {
-      res.status(409).send('Duplication error');
-    }
-    if (err) {
-      next(err);
-    }
-  })(req, res, next);
-});
-
-router.post('/signin', async (req, res, next) => {
-  passport.authenticate('login', async (err, user, info) => {
-    try {
-      if (err || !user) {
-        res.status(401).send('Wrong email or password');
-        return next('Wrong email or password');
-      }
-
-      req.login(user, { session: false }, async (error) => {
-        if (error) return next(error);
-
-        const body = { _id: user._id, email: user.email };
-
-        const token = jwt.sign({ user: body }, 'top_secret');
-
-        return res.json({ token });
-      });
-    } catch (error) {
-      return next(error);
-    }
-  })(req, res, next);
-});
+router.post('/signin', authorizationController.passportAuthenticateLogin);
 
 module.exports = router;
