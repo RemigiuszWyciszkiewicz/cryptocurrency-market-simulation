7870be9205c3ad319bb49ba93a191f5305f82e2d
Remigiusz Wyciszkiewicz <remigiusz.wyciszkiewicz@asseco.pl>
2020-08-07 09:45:40 +0200

    analyitics route tracking created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import {
   CryptocurrenciesQuery,
   CryptocurrenciesService,
@@ -11,6 +12,7 @@ import { filter } from 'rxjs/operators';
 import { AuthService } from './core/authorization';
 import { UserStore } from './data-access/user';
 
+declare let gtag: Function;
 @Component({
   selector: 'coin-market',
   templateUrl: './app.component.html',
@@ -23,15 +25,19 @@ export class AppComponent implements OnInit {
     private readonly _iconLibraries: NbIconLibraries,
     private readonly _nbMenuService: NbMenuService,
     private readonly _authService: AuthService,
-    private readonly _userStore: UserStore
+    private readonly _userStore: UserStore,
+    private readonly _router: Router
   ) {
     this._iconLibraries.registerFontPack('font-awesome', { iconClassPrefix: 'fa', packClass: 'fa' });
   }
 
   ngOnInit(): void {
-    if (!this._cryptocurrenciesQuery.hasEntity()) {
-      this.fetchCryptocurrencies();
-    }
+    this.initializeGoogleAnalyticsTrackling();
+    this.fetchCryptocurrencies();
+    this.startListeningOnLogoutAction();
+  }
+
+  startListeningOnLogoutAction(): void {
     this._nbMenuService
       .onItemClick()
       .pipe(filter(this.filterUserMenuEvents))
@@ -44,9 +50,22 @@ export class AppComponent implements OnInit {
   filterUserMenuEvents(value: NbMenuBag): boolean {
     return value.tag === 'userMenu' && value.item.title === UserMenuOption.LOGOUT;
   }
+
   fetchCryptocurrencies(): void {
-    this._cryptocurrenciesService.getCryptocurrenciesList().subscribe((value) => {
-      this._cryptocurrenciesStore.add(value);
+    if (!this._cryptocurrenciesQuery.hasEntity()) {
+      this._cryptocurrenciesService.getCryptocurrenciesList().subscribe((value) => {
+        this._cryptocurrenciesStore.add(value);
+      });
+    }
+  }
+
+  initializeGoogleAnalyticsTrackling(): void {
+    this._router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        gtag('config', 'UA-174901242-1', {
+          page_path: event.urlAfterRedirects,
+        });
+      }
     });
   }
 }
